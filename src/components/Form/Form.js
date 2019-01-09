e49b3ebc82a3a7072537b53dd6c8f65b76d0fdb8
Nikita Skorodumov <mr-zerx@yandex.ru>
2019-01-09 20:41:49 +0500

    Начал третье задание

---
+++ src/components/Form/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Form.css';
+import Bond from './assets/bond_approve.jpg';
+
+export class Form extends React.Component {
+    render() {
+        return
+            
+    }
+}
