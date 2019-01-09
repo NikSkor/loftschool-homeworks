49056d704196e5775b55f3980900072699b0d521
Nikita Skorodumov <mr-zerx@yandex.ru>
2019-01-10 00:17:43 +0500

    Продолжаем

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './Form.css';
 import Bond from './assets/bond_approve.jpg';
 
+const userJames = {
+    firstName: 'james',
+    lastName: 'bond',
+    password: '007'
+}
 export default class Form extends React.Component {
     state = {
         firstName: '',
         lastName: '',
         password: '',
         errors: {},
+        isLogin: false
       }
 
     changeInput = event => {
@@ -18,67 +24,85 @@ export default class Form extends React.Component {
     submitForm = event => {
         event.preventDefault();
 
-        const errors=['Нужно указать имя','Нужно указать фамилию','Нужно указать пароль'];
+        
+        Object.keys(userJames).forEach(element => {
+            console.log(element);
+        });
+        // if (userJames.firstName === this.state.firstName) {this.setState ({
+        //     isLogin: true
+        // })}
+    //     const errors=['Нужно указать имя','Нужно указать фамилию','Нужно указать пароль'];
 
-        if (this.state.firstName === '') {
-            this.setState({ errors: {
-                firstName: errors[0]
-            } 
-
-                
-            })
-        } 
-        if (this.state.lastName === '') {
-            this.setState({
-
-                
-            })
-        } 
-        if (this.state.password === '') {
-            this.setState({
+    //     if (this.state.firstName === '') {
+    //         this.setState({ errors: {
+    //             firstName: errors[0]
+    //         } 
 
                 
-            })
-        }
+    //         })
+    //     } 
+    //     if (this.state.lastName === '') {
+    //         this.setState({
 
-            this.setState({
-                errors:{}
-            })
+                
+    //         })
+    //     } 
+    //     if (this.state.password === '') {
+    //         this.setState({
+
+                
+    //         })
+    //     }
+
+    //         this.setState({
+    //             errors:{}
+    //         })
 
     }
 
     render() {
-        return (
-            <div className="app-container">
-                <form className="form" onSubmit={this.submitForm}>
-                    <h1>Введите свои данные, агент</h1>
-                    <p key="firstname" className="field">
-                        <label className="field__label" htmlFor="firstname">
-                            <span className="field-label">Имя</span>
-                        </label>
-                        <input className="field__input field-input t-input-firstname" type="text" name="firstName" onChange={this.changeInput} value={this.state.firstName.value} />
-                        <span className="field__error field-error t-error-firstname">{this.state.errors.firstName}</span>
-                    </p>
-                    <p key="lastname" className="field">
-                        <label className="field__label" htmlFor="lastname">
-                            <span className="field-label">Фамилия</span>
-                        </label>
-                        <input className="field__input field-input t-input-lastname" type="text" name="lastName" onChange={this.changeInput} value={this.state.lastName.value} />
-                        <span className="field__error field-error t-error-lastname" >{this.state.errors.lastName}</span>
-                    </p>
-                    <p key="password" className="field">
-                        <label className="field__label" htmlFor="password">
-                            <span className="field-label">Пароль</span>
-                        </label>
-                        <input className="field__input field-input t-input-password" type="text" name="password" onChange={this.changeInput} value={this.state.password.value} />
-                        <span className="field__error field-error t-error-password">{this.state.errors.password}</span>
-                    </p>
-                    <div className="form__buttons">
-                        <input type="submit" value="Проверить" className="button t-submit"/>
-                    </div>
-                </form>
-            </div>
-        )
+        if (this.state.isLogin) {
+            return (
+                <div className="app-container">
+                    <img src={Bond} alt="bond approve" className="t-bond-image"/>
+                </div>
+
+            )
+        } else {
+            return (
+                <div className="app-container">
+                    <form className="form" onSubmit={this.submitForm}>
+                        <h1>Введите свои данные, агент</h1>
+                        <p key="firstname" className="field">
+                            <label className="field__label" htmlFor="firstname">
+                                <span className="field-label">Имя</span>
+                            </label>
+                            <input className="field__input field-input t-input-firstname" type="text" name="firstName" onChange={this.changeInput} value={this.state.firstName.value} />
+                            <span className="field__error field-error t-error-firstname">{this.state.errors.firstName}</span>
+                        </p>
+                        <p key="lastname" className="field">
+                            <label className="field__label" htmlFor="lastname">
+                                <span className="field-label">Фамилия</span>
+                            </label>
+                            <input className="field__input field-input t-input-lastname" type="text" name="lastName" onChange={this.changeInput} value={this.state.lastName.value} />
+                            <span className="field__error field-error t-error-lastname" >{this.state.errors.lastName}</span>
+                        </p>
+                        <p key="password" className="field">
+                            <label className="field__label" htmlFor="password">
+                                <span className="field-label">Пароль</span>
+                            </label>
+                            <input className="field__input field-input t-input-password" type="text" name="password" onChange={this.changeInput} value={this.state.password.value} />
+                            <span className="field__error field-error t-error-password">{this.state.errors.password}</span>
+                        </p>
+                        <div className="form__buttons">
+                            <input type="submit" value="Проверить" className="button t-submit"/>
+                        </div>
+                    </form>
+                </div>
+            )
+        }
+        
+
             
 
     }
