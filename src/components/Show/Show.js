3949b4adfdd07e6d14ba163c5ccce5cf80c5c19f
Nikita Skorodumov <mr-zerx@yandex.ru>
2019-01-11 22:28:24 +0500

    Закончил

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -8,45 +8,46 @@ class Show extends React.Component {
         data: null
     }
     static getDerivedStateFromProps(nextProps, prevState) {
-        return {
-            showId: nextProps.showId
-        }
-    }
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.showId !== this.state.showId) {
-            getShowInfo(this.state.showId).then (info =>{
-                // console.log(info);
-                this.setState ({
-                    data: info
-                })
-                // console.log(this.state);
-                // const poop = {};
-                // Object.keys(info).forEach(elem => {
-                //     poop[elem] = info[elem]
-                // })
-                // this.setState = ({...poop})
-                // console.log(poop);
-                }
-            )
+        if (prevState.showId === nextProps.showId) {
+            return null
+        } else {
+            return {
+                showId: nextProps.showId,
+            }
         }
 
-        
+    }
+    componentDidUpdate(prevProps) {
+    if (prevProps.showId !== this.state.showId) {
+        getShowInfo(this.state.showId).then (info =>{
+            this.setState ({
+                data: info
+            })
+            }
+        )
+    }
     }
     render() {
-        console.log(this.state);
-        return(
-            <div className="show">
-                <img src="" alt="" className="show-image"/>
-                <h2 className="show-label t-show-name">{this.state.showId}</h2>
-                <p className="show-text t-show-genre">
-                    <b>Жанр: </b>
-                </p>
-                <p className="show-text t-show-summary"></p>
-            </div>
+        const {data} = this.state;
+    
+        if (!data) {
+            return(
+                <span className="show-information t-show-info">Шоу не выбрано</span>
+            )
+        } 
+        else {
 
-            
-        )
+            return(
+                <div className="show">
+                    <img src={data.image.medium} alt={data.name} className="show-image"/>
+                    <h2 className="show-label t-show-name">{data.name}</h2>
+                    <p className="show-text t-show-genre">
+                        <b>Жанр: </b>{data.genres.join(', ')}
+                    </p>
+                    <p className="show-text t-show-summary" dangerouslySetInnerHTML={{__html:data.summary}}></p>
+                </div>   
+            )
+        }
     }
 }
 export default Show;
-{/* <span>Шоу не выбрано</span> */}
