f49c9f1e018b4c8097b41ae7ddbe95a0fbbfbaba
Nikita Skorodumov <mr-zerx@yandex.ru>
2019-01-11 14:59:00 +0500

    В процессе

---
+++ src/components/Show/Show.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Show.css';
+
+class Show extends React.Component {
+    state = {
+        showId: '' ,
+        data: null
+    }
+    static getDerivedStateFromProps(nextProps, prevState) {
+
+    }
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+    render() {
+        return(
+
+        )
+    }
+}
+export default Show;
