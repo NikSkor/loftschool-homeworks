95d6ff35eecba927fbd503f4cc76a08c285a380e
Nikita Skorodumov <mr-zerx@yandex.ru>
2019-01-16 01:14:33 +0500

    Допилил

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,16 +4,11 @@ import './Footer.css';
 
 class Footer extends PureComponent {
   render() {
-    const {child} = this.props;
     return <AuthConsumer>{
       ({isAuthorized, email }) => 
-        <footer className='footer'>
-              {child}
-              <p className='footer-message t-footer'>{(isAuthorized) ?'Вы вошли как ' + email :'Вы гость в этой системе'}</p>
-            </footer>
-        }
+        <p className='footer-message t-footer'>{(isAuthorized) ?'Вы вошли как ' + email :'Вы гость в этой системе'}</p>
+      }
       </AuthConsumer>
-
   }
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,19 +5,14 @@ import './Header.css';
 
 class Header extends PureComponent {
   render() {
-    const {child} = this.props;
     return <AuthConsumer>
       {({isAuthorized, email, logout}) => 
-          <header className='header'>
-            {child}
-              {isAuthorized ?
-                <div className='header__content'>
-                    <div className='header-menu'>
-                      <p className='header-menu__email header-email t-header-email'>{email}</p>
-                      <Button className='header-menu__button t-logout' onClick={logout}>'Выйти'</Button>
-                    </div> 
-                </div>:''}
-          </header>
+        (isAuthorized)
+          ?<div className='header-menu'>
+            <p className='header-menu__email header-email t-header-email'>{email}</p>
+            <Button className='header-menu__button t-logout' onClick={logout}>Выйти</Button>
+          </div>
+          : null
       }
    </AuthConsumer>
   }
