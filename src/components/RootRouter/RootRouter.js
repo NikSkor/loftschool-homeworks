eee92c3a2dd849a012e8ae0fafd93307aabf623f
Nikita Skorodumov <mr-zerx@yandex.ru>
2019-01-20 20:04:58 +0500

    Начато

--- src/components/RootRouter/RootRouter.js
+++ src/components/RootRouter/RootRouter.js
@@ -17,6 +17,9 @@ export default () => (
     <AuthProvider>
       <BrowserRouter>
         <Switch>
+          <Route path='/login' component={LoginForm} />
+          <PrivateRoute path='/app' component={AppRouter} />
+          <Redirect to='/login'/>
           {/*
             Добавьте роуты /app и /login.
             Роут /app должен быть доступен 
